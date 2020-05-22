163a21615195b07bc857955b35db0e1ad6d4dcc4
Miguel Paulino <mpaulino710@gmail.com>
2020-05-21 21:42:34 -0400

    Ending section 3

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="El campo username es obligatorio")]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Debe espesificar un password entre 4 y 8 caracteres")]
         public string Password { get; set; }
     }
 }
